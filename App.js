cfc385b652aa12a585ad3285b493b685420e3b85
Rishika Ranjan <115523587+rishikarjn@users.noreply.github.com>
2025-07-18 19:55:59 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,33 +1,62 @@
-
 import './App.css';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import {useState} from 'react'
+
 
 function App() {
+
+const [counter, setCounter] =useState(0);
+
+const handleClick1 =()=>{
+  setCounter(counter+1)
+}
+const handleClick2 =()=>{
+  if(counter === 0){
+    return 0;
+  }
+  else{
+    setCounter(counter -1);
+  }
+}
+
+
+
+
+
+
   return (
-    <div className="App">
-      <div className="logo-box">
-        <img src="./Twitter.png" alt="logo" className="logo" />
-        <h2>Sign In to Twitter</h2>
-        <button>
-          <img src="./Google.png" alt="logo"  />
-          Sign in with Google
-        </button>
-        <button>
-          <img src="./Apple.png" alt="logo"  />
-          Sign in with Google
-        </button>
+    <div className="app">
+      <div className='container'>
+        <div className='total_amount_card' style={{backgroundImage: 'linear-gradient(to right, rgba(253, 230, 90, 100%), rgba(204, 254, 87, 100%))'}}>
+            
+            {/* <div className='right'>
+              </div> */}
 
-        <hr></hr>
-        <span>Or</span>
+              
+              <div className='card_text'>
+                <h3 className='total_amount_heading'>1</h3>
+              </div>
+              
+        </div>
 
-        <form>
-          <input type="text" placeholder="Phone email or usernaame" />
-          <button>Next</button>
+         <form>
 
-          </form>
-        <button>Forget Password</button>
-          <p>
-            Don't Have an account <a>Sign Up</a>
-          </p>
+          <div className='button_collection'>
+
+            <Stack spacing={2} direction="row">
+              <Button onClick={handleClick1} variant="contained" className='btn_one'>+</Button>
+              <Button onClick={handleClick2} variant="contained" className='btn-two'>-</Button>
+              
+            </Stack>
+
+          </div>
+         </form>
+
+
+
+
+
       </div>
     </div>
   );
